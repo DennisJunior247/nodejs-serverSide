c02837eab562f8f2cf21070697f5324ac4bf89bb
dennisjunior247 <dennisjunior247@gmail.com>
2021-04-29 12:45:44 +0100

    started error handeling with express

--- express/app.js
+++ express/app.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const errorController = require("./controllers/errorControler");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 // 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
+app.all("*", (req, res, next) => {
+  next(new AppError(`cant find ${req.originalUrl} on this server`, 404));
 });
-
-
+app.use(errorController);
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
---
+++ express/utils/appError.js
@@ -0,0 +1,12 @@
+class AppError {
+  constructor(message, statusCode) {
+    Super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "failed" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
