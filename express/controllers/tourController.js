ff4a21ca9d1559141cb5a718c7e88391c60e308a
dennisjunior247 <dennisjunior247@gmail.com>
2021-04-30 10:25:53 +0100

    finished error handling section

--- express/controllers/tourController.js
+++ express/controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -31,6 +32,7 @@ exports.getAllTours = catchAsync(async (req, res) => {
 exports.getTour = catchAsync(async (req, res) => {
   const tour = await Tour.findById(req.params.id);
   // Tour.findOne({ _id: req.params.id })
+  if (!tour) return next(new AppError("not found", 404));
 
   res.status(200).json({
     status: "success",
@@ -54,11 +56,13 @@ exports.createTour = catchAsync(async (req, res) => {
   });
 });
 
-exports.updateTour = catchAsync(async (req, res) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
+
+  if (!tour) return next(new AppError("not found", 404));
 
   res.status(200).json({
     status: "success",
@@ -69,7 +73,9 @@ exports.updateTour = catchAsync(async (req, res) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) return next(new AppError("not found", 404));
 
   res.status(204).json({
     status: "success",
--- express/server.js
+++ express/server.js
@@ -2,6 +2,13 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./app");
 
+//catching synchronous error //
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const db = process.env.DATABASE_LOCAL || 8800;
 
@@ -20,6 +27,14 @@ mongoose
 const port = process.env.PORT || 3000;
 const host = "localhost";
 
-app.listen(port, host, () => {
+const server = app.listen(port, host, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION");
+  server.close(() => {
+    process.exit(1);
+  });
 });
