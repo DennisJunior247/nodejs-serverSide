7c03d4d38fd1e866a99ca92604a166b31e012f3b
dennisjunior247 <dennisjunior247@gmail.com>
2024-01-23 22:09:39 +0000

    understood the concept of req and response in node

--- 2-how-node-works/https.js
+++ 2-how-node-works/https.js
@@ -1,8 +1,32 @@
-const http = require('https')
+const http = require("https");
 
-function doOnincoming(req,res){
+const twitters = ["hello", "hi", "Go home", "get back"];
 
+function doOnincoming(req, res) {
+  const response = req.url.slice(8) - 1;
+  res.end(twitters[response]);
+
+ /*   
+ 
+ req = incoming request from the user 
+
+ req.url = returns users url 
+ req.headers = returns users headers containting meta data
+ req.params = url path 
+ req.query = object query 
+ req,method = methods 
+ req.body = body of request 
+ req.is = checks if paths matches  
+ 
+ res = userd to send response to the client 
+ res.setHeader 
+ res.statusCode
+ res.sendFile
+ res.writeHeader
+ res.redirect
+ res.send
+ res.end = end and send response
+ */
 }
 
-const server= http.createServer(doOnincoming)
-
+const server = http.createServer(doOnincoming);
